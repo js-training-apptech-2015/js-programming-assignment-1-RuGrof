a2e89d9bf58db0b95cea1cf7de2e3c952c1108f1
RuGrof <RuGrof@gmail.com>
2015-11-06 10:12:38 +0500

    function solution2 add, complexity O(1)

--- solution2-test.js
+++ solution2-test.js
@@ -1,8 +1,19 @@
-QUnit.test("solution2Naive test", function ( assert ) {
-    for(var i = -35; i<-2; i++){
-        for(var j = -33; j < 0 ; j++){
-            assert.strictEqual(solution2Naive(i, j), solution2Naive(-j, -i), "Passed!" );
+QUnit.test("solution2Naive Negative test", function ( assert ) {
+    for(var i = -8; i<-1; i++){
+        for(var j = i+1; j < 0 ; j++){
+            console.log(i,j);
+            assert.strictEqual(solution2Naive(i, j), solution2(i,j), "Passed!" );
         }
     }
 
 });
+QUnit.test("solution2Naive Positive test", function ( assert ) {
+    for(var i = 1; i<17; i++){
+        for(var j = i+1; j < 19 ; j++){
+            console.log(i,j);
+            assert.strictEqual(solution2Naive(i, j), solution2(i,j), "Passed!" );
+        }
+    }
+
+});
+
--- solution2.js
+++ solution2.js
@@ -1,23 +1,100 @@
 /**
-* returns a maximal XOR value for two numbers between A and B
-* @private
-* @method solution2
-* @param {Number} A
-* @param {Number} B
-* @return {Number} maximal value
-*/
-function solution2Naive(A, B){
+ * returns a maximal XOR value for two numbers between A and B
+ * @private
+ * @method solution2
+ * @param {Number} A
+ * @param {Number} B
+ * @return {Number} maximal value
+ */
+function solution2Naive(A, B) {
     var max;
     var temp;
-    max = A^B;
-    for(var i = B; i>A; i--){
-        for(var j = A; j < i; j++){
-            temp = i^j;
-            if(temp > max){
+    max = A ^ B;
+    for (var i = B; i > A; i--) {
+        for (var j = A; j < i; j++) {
+            temp = i ^ j;
+            if (temp > max) {
                 max = temp;
             }
         }
     }
     return max;
 
+}
+
+/**
+ * returns a maximal XOR value for two numbers between A and B
+ * @private
+ * @method solution2
+ * @param {Number} A
+ * @param {Number} B
+ * @return {Number} maximal value
+ */
+function solution2(A, B) {
+    var max;
+    var min;
+    var resault;
+    var trulyA;
+    var trulyB;
+    var i, j;
+
+    /**
+     * Return Number in directCode
+     * @private
+     * @method directCode
+     * @param {Number} Number in Two's complement
+     * @return {Number} return N
+     */
+    function directCode(num) {
+        var resault;
+
+        resault = (~num);
+
+        return resault;
+    }
+
+    if (A === 0 || B === 0) {
+
+        resault = Math.max(A, B);
+        console
+    } else {
+
+        //Create truly Nubmer
+        if (A < 0) {
+
+            trulyA = directCode(A);
+        } else {
+
+            trulyA = A;
+        }
+        if (B < 0) {
+
+            trulyB = directCode(B);
+        } else {
+
+            trulyB = B;
+        }
+
+
+        max = Math.max(trulyA, trulyB);
+        min = Math.min(trulyA, trulyB);
+        max = max.toString(2);
+        min = min.toString(2);
+
+        if (max.length != min.length) {
+            //different elder Byte
+            resault = Math.pow(2, max.length) - 1;
+
+        } else {
+            console.log(min.length, max.length);
+            for (i = 0; i < min.length; i++) {
+                if (max[i] == "0" && min[i] == "1" || max[i] == "1" && min[i] == "0") {
+                    resault = Math.pow(2, min.length - i) - 1;
+                    break;
+                }
+            }
+        }
+
+    }
+    return resault;
 }
